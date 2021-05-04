cce200f4de2ff2b7fc2e363761fce2e94f7ebae5
Violette Durocher <violette.durocher@etu.utc.fr>
2021-05-04 17:05:55 +0200

    Upload New File

---
+++ drop.sql
@@ -0,0 +1,23 @@
+-- Suppression de la base de données 
+
+DROP TABLE Agence CASCADE;
+DROP TABLE Employe CASCADE;
+DROP TABLE AgentTechnique CASCADE;
+DROP TABLE AgentCommercial CASCADE;
+DROP TABLE SocieteEntretien CASCADE;
+DROP TABLE AssocAgenceSocieteEntretien CASCADE;
+DROP TABLE Entretien CASCADE;
+DROP TABLE Option CASCADE;
+DROP TABLE TypeCarburant CASCADE;
+DROP TABLE Marque CASCADE;
+DROP TABLE CategorieVehicule CASCADE;
+DROP TABLE Modele CASCADE;
+DROP TABLE Vehicule CASCADE;
+DROP TABLE Location CASCADE;
+DROP TABLE Particulier RESTART IDENTITY CASCADE;
+DROP TABLE LocationParticulier CASCADE;
+DROP TABLE Entreprise CASCADE;
+DROP TABLE Conducteur CASCADE;
+DROP TABLE LocationProfessionnel CASCADE;
+DROP TABLE Facturation CASCADE;
+DROP TABLE ValidationFinale CASCADE;
