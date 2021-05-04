78fa99b0627124a791035ca96481684b75493945
Thomas Lerner <thomas.lerner@etu.utc.fr>
2021-05-04 11:43:30 +0200

    Update delete.sql Ajout Cascade

--- delete.sql
+++ delete.sql
@@ -1,26 +1,26 @@
 -- Cas où l'on veut simplement vider les tables, sans les supprimer
 
-TRUNCATE TABLE 'Agence';
-TRUNCATE TABLE 'Employe';
-TRUNCATE TABLE 'AgentTechnique';
-TRUNCATE TABLE 'AgentCommercial';
-TRUNCATE TABLE 'SocieteEntretien';
-TRUNCATE TABLE 'AssocAgenceSocieteEntretien';
-TRUNCATE TABLE 'Entretien';
-TRUNCATE TABLE 'Option';
-TRUNCATE TABLE 'TypeCarburant';
-TRUNCATE TABLE 'Marque';
-TRUNCATE TABLE 'CategorieVehicule';
-TRUNCATE TABLE 'Modele';
-TRUNCATE TABLE 'Vehicule';
-TRUNCATE TABLE 'Location';
-TRUNCATE TABLE 'Particulier';
-TRUNCATE TABLE 'LocationParticulier';
-TRUNCATE TABLE 'Entreprise';
-TRUNCATE TABLE 'Conducteur';
-TRUNCATE TABLE 'LocationProfessionnel';
-TRUNCATE TABLE 'Facturation';
-TRUNCATE TABLE 'ValidationFinale';
+TRUNCATE TABLE Agence CASCADE;
+TRUNCATE TABLE Employe CASCADE;
+TRUNCATE TABLE AgentTechnique CASCADE;
+TRUNCATE TABLE AgentCommercial CASCADE;
+TRUNCATE TABLE SocieteEntretien CASCADE;
+TRUNCATE TABLE AssocAgenceSocieteEntretien CASCADE;
+TRUNCATE TABLE Entretien CASCADE;
+TRUNCATE TABLE Option CASCADE;
+TRUNCATE TABLE TypeCarburant CASCADE;
+TRUNCATE TABLE Marque CASCADE;
+TRUNCATE TABLE CategorieVehicule CASCADE;
+TRUNCATE TABLE Modele CASCADE;
+TRUNCATE TABLE Vehicule CASCADE;
+TRUNCATE TABLE Location CASCADE;
+TRUNCATE TABLE Particulier CASCADE;
+TRUNCATE TABLE LocationParticulier CASCADE;
+TRUNCATE TABLE Entreprise CASCADE;
+TRUNCATE TABLE Conducteur CASCADE;
+TRUNCATE TABLE LocationProfessionnel CASCADE;
+TRUNCATE TABLE Facturation CASCADE;
+TRUNCATE TABLE ValidationFinale CASCADE;
 
 -- Cas où l'on veut supprimer toute la base de données et toutes ses données
 
