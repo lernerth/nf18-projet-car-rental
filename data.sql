37e53d080390278771678afd68b2d92cee5f1e6f
Violette Durocher <violette.durocher@etu.utc.fr>
2021-05-04 17:17:19 +0200

    Update data.sql

--- data.sql
+++ data.sql
@@ -63,16 +63,11 @@ VALUES
 ('GG123GG', 'golf', 'essence', 'noir', 1200, 1, 1),
 ('HH123HH', 'golf', 'gasoil', 'gris', 4000, 1, 2);
 
-
-
-
 INSERT INTO Particulier(nom,prenom,num_bancaire,mail ,telephone ,num_permis, date_naissance) 
 VALUES
 ('Albert','Pinot', '0000111122223333', 'albertpinot@gmail.com', '0695605788','20AW24096', '07/07/1998'),
 ('Charles','Renard', '1111222200003333', 'charlesrenard@live.fr', '0678607898','19AW24696', '03/07/1999'),
 ('Louis','Barre', '1111777700003333', 'louisbarre@live.fr', '0679907898','19AW28896', '03/08/1999');
-
-
 
 INSERT INTO Entreprise(nom,num_bancaire,mail,tel,siret) 
 VALUES 
@@ -88,24 +83,24 @@ INSERT INTO Conducteur(num_permis, nom, prenom, date_naissance, entreprise) VALU
 	('039527393', 'Sombrero', 'Antony', '06/05/1976', 1),
 	('103846389', 'Geigberg', 'David', '12/12/1992', 1),
 	('109372573', 'Oui-Oui', 'Medhi', '25/12/1994', 2);
-
-
-INSERT INTO Facturation(idfacturation, clientParticulier, clientProfessionnel, agent_com,
-                        montant, date_payement, moyen_reglement, etat_payement) 
-        VALUES  (1, NULL, '182640721', 1, 1200, '12-09-2020', 'CB',     TRUE), 
-                (2, NULL, '039527393', 2, 1500, '12-10-2020', 'CB',     TRUE),
-				(3, NULL, '039527393', 2, 1500, '12-10-2020', 'CB',     TRUE),)
+	
+INSERT INTO Facturation(idfacturation, clientParticulier, clientProfessionnel, agent_com, montant, date_payement, moyen_reglement, etat_payement) 
+VALUES  
+(1, NULL, '182640721', 1, 1200, '12-09-2020', 'CB', TRUE), 
+(2, NULL, '039527393', 2, 1500, '12-10-2020', 'CB', TRUE),
+(3, NULL, '039527393', 2, 1500, '12-10-2020', 'CB', TRUE);
 
 INSERT INTO ValidationFinale(agent_com, location, date_validation, resultat_validation)
-        VALUES  (1, 1, '15-1-2021', TRUE),
-                (1, 1, '13-1-2021', TRUE),
-                (2, 1, '14-1-2021', TRUE);
+VALUES  
+(1, 1, '15-1-2021', TRUE),
+(1, 1, '13-1-2021', TRUE),
+(2, 1, '14-1-2021', TRUE);
 
-INSERT INTO Entretien(identretien, date_entretien, date_controle, 
-                        resultat, societe, agent_tech)
-        VALUES  (1, '20-01-2021', '21-01-2021', 'abc', '36252187900034', 1),
-                (2, '20-02-2021', '21-03-2021', 'xyz', '55252187900021', 2),
-                (3, '20-03-2021', '21-05-2021', 'xyz', '36252187900034', 1);
+INSERT INTO Entretien(identretien, date_entretien, date_controle, resultat, societe, agent_tech)
+VALUES  
+(1, '20-01-2021', '21-01-2021', 'abc', '36252187900034', 1),
+(2, '20-02-2021', '21-03-2021', 'xyz', '55252187900021', 2),
+(3, '20-03-2021', '21-05-2021', 'xyz', '36252187900034', 1);
 
 INSERT INTO Location(date_debut ,date_fin,km_parcourus,vehicule_immat,entretien ,facturation ) VALUES 
 ('05/08/2020', '12/12/2020',3000,'AA123AA', 1,1),
