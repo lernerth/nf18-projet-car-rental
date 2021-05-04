57db8276399506dfe8681de9101bb04ee60a004a
Violette Durocher <violette.durocher@etu.utc.fr>
2021-05-04 18:36:30 +0200

    Update data.sql

--- data.sql
+++ data.sql
@@ -16,14 +16,14 @@ VALUES
 
 INSERT INTO AgentCommercial
 VALUES
-(3),(4);
+(3),(4),(5);
 
 INSERT INTO SocieteEntretien 
 VALUES 
 ('36252187900034', 'Carglass'),
 ('55252187900021', 'EpongeOmax');
 
-INSERT INTO AssocAgenceSocieteEntretien 
+INSERT INTO AssocAgenceSocieteEntretien(id_agence, siret)
 VALUES 
 (1,'36252187900034'),
 (1,'55252187900021'); 
@@ -63,13 +63,13 @@ VALUES
 ('GG123GG', 'golf', 'essence', 'noir', 1200, 1, 1),
 ('HH123HH', 'golf', 'gasoil', 'gris', 4000, 1, 2);
 
-INSERT INTO Particulier(nom,prenom,num_bancaire,mail ,telephone ,num_permis, date_naissance) 
+INSERT INTO Particulier(nom, prenom, num_bancaire, mail, telephone, num_permis, date_naissance) 
 VALUES
 ('Albert','Pinot', '0000111122223333', 'albertpinot@gmail.com', '0695605788','20AW24096', '07/07/1998'),
 ('Charles','Renard', '1111222200003333', 'charlesrenard@live.fr', '0678607898','19AW24696', '03/07/1999'),
 ('Louis','Barre', '1111777700003333', 'louisbarre@live.fr', '0679907898','19AW28896', '03/08/1999');
 
-INSERT INTO Entreprise(nom,num_bancaire,mail,tel,siret) 
+INSERT INTO Entreprise(nom, num_bancaire, mail, tel, siret) 
 VALUES 
 ('Nike','1234111122223333','nike@gmail.com','0646734566','11122294112344'),
 ('Zumbacafew','2223133356778444','zbc.jul@gmail.com','0744532458','33523256623454'),
@@ -77,25 +77,16 @@ VALUES
 ('Tchikita','4466443277664344','tchiki.tchiki@orange.fr','0677665454','33455566547654'),
 ('Machine de A','5544644433234444','quedescracks@gmail.com','0788665644','00322274431467');
 
-INSERT INTO Conducteur(num_permis, nom, prenom, date_naissance, entreprise) VALUES 
+INSERT INTO Conducteur(num_permis, nom, prenom, date_naissance, entreprise) 
+VALUES 
 	('182640721', 'Dubois', 'Paul', '19/10/1970', 1),
-	('183946253', 'Dupont', 'Etienne', '23/01/1986', 2),
-	('039527393', 'Sombrero', 'Antony', '06/05/1976', 1),
-	('103846389', 'Geigberg', 'David', '12/12/1992', 1),
-	('109372573', 'Oui-Oui', 'Medhi', '25/12/1994', 2);
+	('203946253', 'Dupont', 'Etienne', '23/01/1986', 1),
+	('039527393', 'Sombrero', 'Antony', '06/05/1976', 2),
+	('103846389', 'Geigberg', 'David', '12/12/1992', 3),
+	('109372573', 'Oui-Oui', 'Medhi', '25/12/1994', 4);
 
-INSERT INTO ValidationFinale(agent_com, location, date_validation, resultat_validation)
-VALUES  
-(1, 1, '15-1-2021', TRUE),
-(1, 1, '13-1-2021', TRUE),
-(2, 1, '14-1-2021', TRUE);
 
-INSERT INTO Entretien(identretien, date_entretien, date_controle, resultat, societe, agent_tech)
-VALUES  
-(1, '20-01-2021', '21-01-2021', 'abc', '36252187900034', 1),
-(2, '20-02-2021', '21-03-2021', 'xyz', '55252187900021', 2),
-(3, '20-03-2021', '21-05-2021', 'xyz', '36252187900034', 1);
-
+-- ******* --
 INSERT INTO Location(date_debut ,date_fin,km_parcourus,vehicule_immat,entretien ,facturation ) VALUES 
 ('05/08/2020', '12/12/2020',3000,'AA123AA', 1,1),
 ('07/03/2020', '06/01/2021',400,'DD123DD', 2,2),
@@ -111,6 +102,18 @@ INSERT INTO LocationParticulier(id_contrat, particulier )
 VALUES
 (1,1),
 (2,3);
+
+INSERT INTO ValidationFinale(agent_com, location, date_validation, resultat_validation)
+VALUES  
+(3, 1, '15-1-2021', TRUE),
+(3, 2, '13-1-2021', TRUE),
+(4, 3, '14-1-2021', TRUE);
+
+INSERT INTO Entretien(identretien, date_entretien, date_controle, resultat, societe, agent_tech)
+VALUES  
+(1, '20-01-2021', '21-01-2021', 'abc', '36252187900034', 1),
+(2, '20-02-2021', '21-03-2021', 'xyz', '55252187900021', 2),
+(3, '20-03-2021', '21-05-2021', 'xyz', '36252187900034', 1);
 
 -- INSERT INTO Facturation(idfacturation, clientParticulier, clientProfessionnel, agent_com, montant, date_payement, moyen_reglement, etat_payement) 
 --VALUES  
