a30ec6686969550272b5e322061c99051cc26a3e
Nguyen Le Ly Bang <bangjdev@gmail.com>
2021-05-14 17:49:15 +0200

    reformat

--- application/app.py
+++ application/app.py
@@ -9,32 +9,44 @@ from decimal import Decimal
 """
     Connecter à la base de données
 """
-conn = psycopg2.connect(dbname=cfg.DBNAME, user=cfg.DBUSER, password=cfg.DBPWD, host=cfg.DBHOST, port=cfg.DBPORT)
+conn = psycopg2.connect(dbname=cfg.DBNAME, user=cfg.DBUSER,
+                        password=cfg.DBPWD, host=cfg.DBHOST, port=cfg.DBPORT)
 curseur = conn.cursor()
 
 """
     Afficher un menu
     Retourner une fonction qui réalise l'option choisie par utilisateur
 """
+
+
 def menu(menu_items):
     for item in menu_items:
         print(item, ". ", menu_items[item][0], sep="", end="\n")
     return menu_items[input("> ")][-1]
 
+
 """
     Retrouver toutes les lignes d'un table
 """
+
+
 def select_all(table):
     query = "SELECT * FROM %s" % table
     curseur.execute(query)
     return curseur.fetchall()
+
+
 """
     Insérer des données
 """
+
+
 def insert(table, colonnes, valeurs):
-    query = "INSERT INTO %s(%s) VALUES(%s);" % (table, ",".join(colonnes), ",".join(["%s"] * len(colonnes)))
+    query = "INSERT INTO %s(%s) VALUES(%s);" % (
+        table, ",".join(colonnes), ",".join(["%s"] * len(colonnes)))
     curseur.execute(query, valeurs)
     conn.commit()
+
 
 def ajouter_vehicule():
     valeurs = [
@@ -46,28 +58,34 @@ def ajouter_vehicule():
         input("\tID agence ? "),
         input("\tID d'agent technique : ")
     ]
-    insert("Vehicule", ["immat", "modele", "carburant", "couleur", "nb_km", "agence", "agent_tech"], valeurs)
+    insert("Vehicule", ["immat", "modele", "carburant",
+           "couleur", "nb_km", "agence", "agent_tech"], valeurs)
+
+
 """
     Afficher des tables
 """
+
+
 def afficher_agences():
     for row in select_all("Agence"):
         print(row)
+
 
 def afficher_vehicules():
     for row in select_all("Vehicule"):
         print(row)
 
 
-
 """
     Quitter programme
 """
+
+
 def quitter():
     curseur.close()
     conn.close()
     exit(0)
-
 
 
 # Définir listes des menus ici
@@ -85,6 +103,7 @@ menu_items = {
     "7": ["...", ...],
     "0": ["Quitter le programme", quitter]
 }
+
 
 def main():
     while True:
