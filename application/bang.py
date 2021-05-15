d9f7167ccb7a4f22befd61add03a0bb279a5cf62
NGUYEN Le Ly Bang <le-ly-bang.nguyen@etu.utc.fr>
2021-05-15 22:10:33 +0200

    Fix argent_rapporte requete

--- application/bang.py
+++ application/bang.py
@@ -1,34 +1,28 @@
 # Bilan et statistiques par catégorie
 
 def bilan_par_categorie(curseur):
-    query =  """SELECT 	total.categorie AS categorie,
-                        total.nb_vehicules AS nb_vehicules,
-                        encours.nb_vehicules AS nb_vehicules_en_location,
-                        total.pourcentage,
-                        encours.argent_rapporte
+    query =  """SELECT 	total.categorie, total.nb_vehicules, encours.nb_vehicules AS nb_vehicules_en_location,
+                        total.pourcentage AS pourcentage_total_vehicules, statistique.argent_rapporte
                 FROM
                     (SELECT CategorieVehicule.nom as categorie, COUNT(Vehicule.immat) as nb_vehicules, (COUNT(Vehicule.immat) * 100.0 / SUM(COUNT(Vehicule.immat)) over ()) as pourcentage FROM CategorieVehicule
                         JOIN Modele ON Modele.categorie = CategorieVehicule.nom
                         JOIN Vehicule ON Modele.nom = Vehicule.modele
                         GROUP BY CategorieVehicule.nom) AS total
                 JOIN (SELECT 	CategorieVehicule.nom as categorie,
-                                SUM(CASE
-                                        WHEN Location.date_debut <= current_date AND current_date <= Location.date_fin
-                                        THEN 1
-                                        ELSE 0
-                                    END) as nb_vehicules,
-                                SUM(CASE Facturation.etat_payement
-                                        WHEN TRUE
-                                        THEN Facturation.montant
-                                        ELSE Money(0)
-                                    END) as argent_rapporte
+                                SUM(CASE WHEN Location.date_debut <= current_date AND current_date <= Location.date_fin THEN 1 ELSE 0 END) as nb_vehicules
                     FROM CategorieVehicule
                         JOIN Modele ON Modele.categorie = CategorieVehicule.nom
                         JOIN Vehicule ON Modele.nom = Vehicule.modele
                         JOIN Location ON Location.vehicule_immat = Vehicule.immat
-                        JOIN Facturation ON Facturation.idfacturation = Location.facturation
-                        GROUP BY CategorieVehicule.nom) AS encours
-                ON total.categorie = encours.categorie;"""
+                        GROUP BY CategorieVehicule.nom) AS encours ON total.categorie = encours.categorie
+                JOIN (SELECT argent.categorie, SUM(argent.montant) as argent_rapporte FROM
+                        (SELECT DISTINCT CategorieVehicule.nom as categorie, Facturation.montant  FROM Location
+                            JOIN Facturation ON Facturation.idfacturation = Location.facturation
+                            JOIN Vehicule ON Vehicule.immat = Location.vehicule_immat
+                            JOIN Modele ON Vehicule.modele = Modele.nom
+                            JOIN CategorieVehicule ON CategorieVehicule.nom = Modele.categorie
+                            WHERE Facturation.etat_payement = TRUE) AS argent
+                    GROUP BY argent.categorie) AS statistique ON statistique.categorie = encours.categorie;"""
     curseur.execute(query)
     resultats = curseur.fetchall()
     for row in resultats:
