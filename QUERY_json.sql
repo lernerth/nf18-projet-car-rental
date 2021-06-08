13fccdd7ac96a7a470556ddad4c17ea8f03570c8
Noe Redouin <noe.redouin@etu.utc.fr>
2021-06-08 10:56:20 +0000

    Update QUERY_json.sql

--- QUERY_json.sql
+++ QUERY_json.sql
@@ -6,3 +6,7 @@ FROM Vehicule v, JSON_ARRAY_ELEMENTS(v.option) opt;
 -- trouver la catégorie d un véhicule
 SELECT CAST(v.immat,v.modele->categorie->>"nom" AS VARCHAR) AS categorie,CAST(v.modele->categorie->>"description" AS VARCHAR) AS description
 FROM Vehicule v;
+
+-- trouver le modele d un véhicule
+SELECT CAST(v.immat,v.modele->>"nom" AS VARCHAR) AS modele,CAST(v.modele->>"nb_portes" AS VARCHAR) AS nbre_portes
+FROM Vehicule v;
