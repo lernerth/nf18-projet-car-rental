1ae16328801823ee4da2d85f53c92b8df9a1e73c
Noe Redouin <noe.redouin@etu.utc.fr>
2021-06-08 10:52:56 +0000

    Update QUERY_json.sql

--- QUERY_json.sql
+++ QUERY_json.sql
@@ -1,4 +1,8 @@
 -- 
 
-SELECT v.nom, opt.* -- permet d' afficher toute les options pour un même véhicule
+SELECT v.immat, opt.* -- permet d' afficher toute les options pour un même véhicule
 FROM Vehicule v, JSON_ARRAY_ELEMENTS(v.option) opt;
+
+-- trouver la catégorie d un véhicule
+SELECT CAST(v.modele->categorie->>"nom" AS VARCHAR) AS categorie,CAST(v.modele->categorie->>"description" AS VARCHAR) AS description
+FROM Vehicule v;
