8e16ca4ee9150a12286612cb83ca1a7ef8e43555
Violette Durocher <violette.durocher@etu.utc.fr>
2021-06-08 10:39:55 +0000

    Upload New File

---
+++ DATA_json.sql
@@ -0,0 +1,14 @@
+-- 
+
+INSERT INTO Vehicule (immat, modele, carburant, couleur, nb_km, agence, agent_tech) 
+VALUES ( 
+‘AA123AA’,
+'{“nom":"C4", "nb_portes":4, "marque" : { "nom":"citroen"}, 
+	"categorie" : {"nom" : "berline", "description":"voiture de ville"	}}',
+'{"liste_options" : ["climatisation","limiteur de vitesse",”sieges massants”]}'
+‘essence’,
+‘bleu’,
+3000,
+1,
+2
+);
