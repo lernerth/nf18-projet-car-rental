ca947ec4e180c99c26694d20dafd9ead6ffe546e
Noe Redouin <noe.redouin@etu.utc.fr>
2021-06-15 09:50:52 +0000

    Update CREATE_json.sql

--- CREATE_json.sql
+++ CREATE_json.sql
@@ -46,3 +46,18 @@ CREATE TABLE Vehicule(
   FOREIGN KEY(agent_tech) REFERENCES AGENTTECHNIQUE(id_employe),
   CONSTRAINT check_immat CHECK(immat SIMILAR TO '[0-9A-Z]{7}')
 );
+CREATE TABLE Location(
+  id_contrat SERIAL PRIMARY KEY,
+  date_debut DATE NOT NULL,
+  date_fin DATE,
+  km_parcourus DECIMAL NOT NULL,
+  vehicule_immat VARCHAR(7) NOT NULL,
+  entretien INTEGER UNIQUE NOT NULL,
+  facturation INTEGER NOT NULL,
+  agent_com INTEGER NOT NULL,
+  validationFinale JSON,
+  FOREIGN KEY(vehicule_immat) REFERENCES Vehicule(immat),
+  FOREIGN KEY(entretien) REFERENCES Entretien(id_entretien),
+  FOREIGN KEY(facturation) REFERENCES Facturation(idFacturation),
+  FOREIGN KEY(agent_com) REFERENCES AgentCommercial(id_employe)
+);
