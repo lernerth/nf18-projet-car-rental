30b33b5b489be863f19197e1311b2d5abe141027
Noe Redouin <noe.redouin@etu.utc.fr>
2021-06-15 10:30:53 +0000

    Update json.py

--- application/json.py
+++ application/json.py
@@ -1,8 +1,9 @@
 
-
-def option_vehicule(client):
-    sql = "SELECT SUM(montant) FROM Facturation JOIN Entreprise ON Facturation.ClientProfessionnel=Entreprise.id_client WHERE Entreprise.id_client = %s AND Facturation.etat_payement = TRUE"
+def option_vehicule():
+    sql = "SELECT v.immat, opt.* FROM Vehicule v, JSON_ARRAY_ELEMENTS(v.liste_options) opt"
     curseur.execute(sql, (client,))
-    result = curseur.fetchone()
-    option = result[0]
-    return paye_pro
+    raw = curseur.fetchone()
+    while raw:
+        print ("immatriculation: ", raw[0], "option:", raw[1])
+        raw = curseur.fetchone()
+
