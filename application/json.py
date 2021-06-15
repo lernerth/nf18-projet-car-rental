bd765ff997f13b2296f70727e08f1d6a43297d07
Noe Redouin <noe.redouin@etu.utc.fr>
2021-06-15 10:24:13 +0000

    Update json.py

--- application/json.py
+++ application/json.py
@@ -1,1 +1,8 @@
 
+
+def option_vehicule(client):
+    sql = "SELECT SUM(montant) FROM Facturation JOIN Entreprise ON Facturation.ClientProfessionnel=Entreprise.id_client WHERE Entreprise.id_client = %s AND Facturation.etat_payement = TRUE"
+    curseur.execute(sql, (client,))
+    result = curseur.fetchone()
+    option = result[0]
+    return paye_pro
