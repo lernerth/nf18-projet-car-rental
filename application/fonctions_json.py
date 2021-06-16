c74b1588b0a8e03e723510743dc10f28598291cd
NGUYEN Le Ly Bang <le-ly-bang.nguyen@etu.utc.fr>
2021-06-16 12:53:00 +0200

    remove wrong param

--- application/fonctions_json.py
+++ application/fonctions_json.py
@@ -3,7 +3,7 @@ import json
 
 def option_vehicule():
     sql = "SELECT v.immat, opt.* FROM Vehicule v, JSON_ARRAY_ELEMENTS(v.liste_options) opt"
-    curseur.execute(sql, (client,))
+    curseur.execute(sql)
     raw = curseur.fetchone()
     while raw:
         print("immatriculation: ", raw[0], "option:", raw[1])
